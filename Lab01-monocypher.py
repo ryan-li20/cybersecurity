9858ac88c48991ad0a6ff2925e9f63e1a75a8e5e
ryan-li20 <rli20@stuy.edu>
2021-09-27 23:08:45 -0400

    added algorith; works for singular strings

--- Lab01-monocypher.py
+++ Lab01-monocypher.py
@@ -1,3 +1,22 @@
-def Monocypher():
+def monocypher():
     string = input("Encrypted String")
-    
+    letters = {}
+    letterAmount = 0
+    for x in range(26):
+        letters[chr(ord('a') + x)] = 0  # add letters a-z as keys and 0 as init value
+
+    for element in string:
+        for letter in letters.keys():
+            if element == letter:
+                letters.update({element: letters[letter] + 1})
+                letterAmount += 1
+                break
+    for letter in letters:
+        letters.update({letter: letters[letter] / letterAmount})
+
+    for x, y in letters.items():  # print out the dictionary in correct format
+        print(x, ": ", y)
+    print(letterAmount)
+
+
+monocypher()

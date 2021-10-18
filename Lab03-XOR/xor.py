26e8bf72d30d09c1e8f0cd48946c55055edb33ec
ryan-li20 <rli20@stuy.edu>
2021-10-18 15:30:39 -0400

    nothing fin

---
+++ Lab03-XOR/xor.py
@@ -0,0 +1,17 @@
+import sys
+mode = sys.argv[1]
+keyfile = sys.argv[2]
+inpfile = sys.argv[3]
+key = open(keyfile,"rb").read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
+inp = open(inpfile,"rb").read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
+debug = False
+
+if(debug):
+  print("mode:"+mode)
+  print("key: "+key)
+  print("inp: "+inp)
+
+def human(text, key):
+    textCount = 0
+    for char in text:
+        ord(text) + ord(textCount/len(key))

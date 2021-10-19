e51331ddeb6773e22ef9cd255c614f239b997659
ryan-li20 <rli20@stuy.edu>
2021-10-18 22:09:48 -0400

    added human readable format, added keyfile input

--- Lab03-XOR/xor.py
+++ Lab03-XOR/xor.py
@@ -1,9 +1,9 @@
 import sys
-mode = sys.argv[1]
-keyfile = sys.argv[2]
-inpfile = sys.argv[3]
-key = open(keyfile,"rb").read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
-inp = open(inpfile,"rb").read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
+# mode = sys.argv[1]
+# keyfile = sys.argv[2]
+# inpfile = sys.argv[3]
+# key = open(keyfile,"rb").read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
+# inp = open(inpfile,"rb").read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
 debug = False
 
 if(debug):
@@ -14,11 +14,13 @@ if(debug):
 def human(inp, key):
     while len(inp) > len(key):
         key = key + key
+    output = ""
     count = 0
     for char in inp:
-        xor(char, key[count])
+        output = output + chr(xor(char, key[count]))
+    return output
         
-def xor(l1,l2):
+def xor(l1,l2): # given two characters, return the decimal value of them xored
     num1 = ord(l1)
     num2 = ord(l2)
     return num1 ^ num2
